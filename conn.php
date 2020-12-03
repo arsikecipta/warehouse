b8f0e523ec1a3f5e1d5fd484afaa79247d4e1fe0
arsikecipta <arsikepelangi15@gmail.com>
2020-12-03 21:01:22 +0700

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost","root", "","warehouse");
+$conn = mysqli_connect("sql12.freesqldatabase.com","sql12380038", "UVRHmH23B7","sql12380038");
 
 if(!$conn){
     die("Koneksi gagal");
